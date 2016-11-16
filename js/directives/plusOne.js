e13b34f2515e6af926b15dd82d626151e48eab59
Angel yang <angelyang@Angels-MacBook-Pro.local>
2016-11-16 11:22:23 -0800

    first commit

---
+++ js/directives/plusOne.js
@@ -0,0 +1,13 @@
+app.directive('plusOne', function(){
+  return {
+    scope: {
+      
+    },
+    templateUrl: 'js/directives/plusOne.html',
+    link: function(scope, element, attrs) {
+      scope.like = function() {
+        element.toggleClass('btn-like');
+      };
+    }
+  };
+});
